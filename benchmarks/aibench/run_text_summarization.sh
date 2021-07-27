559bec403ccef1b97e755f1dac8d09bc2f0543a2
Hariharan Devarajan <mani.hariharan@gmail.com>
2021-07-27 14:38:27 -0700

    added text summization

---
+++ benchmarks/aibench/run_text_summarization.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+### LSF syntax
+#BSUB -nnodes 1             #number of nodes
+#BSUB -W 15                 #walltime in minutes
+#BSUB -G asccasc           #account
+#BSUB -J cosmoflow          #name of job
+#BSUB -q pdebug             #queue to use
+
+source $PWD/setup_text_summarization.sh
+#jsrun --np 1 \
+cd code/TensorFlow/Text_summarization/
+./run.sh
